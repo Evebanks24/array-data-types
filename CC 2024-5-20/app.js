85b2938414021eb21fc0c5524fe9f64f192df860
Evetta Banks <evettawashington@yahoo.com>
2024-05-20 17:32:28 -0500

    Add coding challenge for 2024-5-20 challenge.

---
+++ CC 2024-5-20/app.js
@@ -0,0 +1,28 @@
+//Task
+//I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+//Answer//
+
+function getLength(arr){
+    return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length-1]
+}
+function pushElement(arr){
+  arr.push(1);
+  return arr
+}
+function popElement(arr){
+    arr.pop()
+    return arr
+}
+
+
+
+
